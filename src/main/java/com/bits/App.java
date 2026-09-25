11ba44c7090e9127b6140cbdf180294329cfdd80
yash2022wb86542 <2022wb86542@wilp.bits-pilani.ac.in>
2026-09-25 14:20:37 +0530

    Update message for CI trigger test

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,6 +2,6 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from the DevOps CI lab - build v1");
+        System.out.println("Hello from the DevOps CI lab - build v2 - triggered by CI");
     }
 }
